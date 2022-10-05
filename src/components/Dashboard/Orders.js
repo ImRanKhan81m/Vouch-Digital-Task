3c9f615da43192ea9b45e20eb38857665e413889
Md Imran Hossen <freelancerimran81@gmail.com>
2022-10-05 14:07:13 +0600

    dashboard updated

--- src/components/Dashboard/Orders.js
+++ src/components/Dashboard/Orders.js
@@ -5,47 +5,25 @@ import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
 import Title from './Title';
 
 // Generate Order Data
-function createData(id, date, name, shipTo, paymentMethod, amount) {
-  return { id, date, name, shipTo, paymentMethod, amount };
+function createData(id, date, name, shipTo, paymentMethod, amount, sites, Tenants, Group, Action) {
+  return { id, date, name, shipTo, paymentMethod, amount, sites, Tenants , Group, Action};
 }
 
 const rows = [
-  createData(
-    0,
-    '16 Mar, 2019',
-    'Elvis Presley',
-    'Tupelo, MS',
-    'VISA ⠀•••• 3719',
-    312.44,
-  ),
-  createData(
-    1,
-    '16 Mar, 2019',
-    'Paul McCartney',
-    'London, UK',
-    'VISA ⠀•••• 2574',
-    866.99,
-  ),
-  createData(2, '16 Mar, 2019', 'Tom Scholz', 'Boston, MA', 'MC ⠀•••• 1253', 100.81),
-  createData(
-    3,
-    '16 Mar, 2019',
-    'Michael Jackson',
-    'Gary, IN',
-    'AMEX ⠀•••• 2000',
-    654.39,
-  ),
-  createData(
-    4,
-    '15 Mar, 2019',
-    'Bruce Springsteen',
-    'Long Branch, NJ',
-    'VISA ⠀•••• 5919',
-    212.79,
-  ),
+  createData(1, 'Infosys', 'management@infosys.com', '+91 9563214587', 'Abhishek Kumar'," - - ", 12 ,'- -','- -'),
+  createData(2, 'Wipro', 'management@wipro.com', '+91 9563214587', 'Abhishek Kumar'," - - ", 8 ,'- -','- -'),
+  createData(3, 'Accenture', 'management@accenture.com', '+91 9563214587', 'Abhishek Kumar'," - - ", 4 ,'- -','- -'),
+  createData(4, 'Capgemini', 'management@capgemini.com', '+91 9563214587', 'Abhishek Kumar'," - - ", 6 ,'- -', 24),
+  createData(5, 'Lodha', 'management@lodha.com', '+91 9563214587', 'Abhishek Kumar'," - - ", 24, 16,'- -'),
+  createData(6, 'HCL', 'management@hcl.com', '+91 9563214587', 'Abhishek Kumar'," - - ", 10 ,'- -',2),
+  createData(7, 'Redington', 'management@redington.com', '+91 9563214587', 'Abhishek Kumar'," - - ", 5, 2,31),
+  createData(8, 'Mphasis', 'management@mphasis.com', '+91 9563214587', 'Abhishek Kumar'," - - ", 1, 23,'- -'),
+  createData(9, 'Larsen & Toubro', 'management@larsentoubro.com', '+91 9563214587', 'Abhishek Kumar'," - - ", 9, '- -','- -'),
+  createData(10, 'Tata Consultancy', 'management@tcs.com', '+91 9563214587', 'Abhishek Kumar'," - - ", 16 ,'- -','- -'),
 ];
 
 function preventDefault(event) {
@@ -55,15 +33,20 @@ function preventDefault(event) {
 export default function Orders() {
   return (
     <React.Fragment>
-      <Title>Recent Orders</Title>
-      <Table size="small">
+      <h4 className='fw-bold'>View Clients</h4>
+      <p><span className='fw-bold'>Client Master </span>/ View Clients</p>
+      <Table >
         <TableHead>
           <TableRow>
-            <TableCell>Date</TableCell>
-            <TableCell>Name</TableCell>
-            <TableCell>Ship To</TableCell>
-            <TableCell>Payment Method</TableCell>
-            <TableCell align="right">Sale Amount</TableCell>
+            <TableCell>Company Name</TableCell>
+            <TableCell>Email Address</TableCell>
+            <TableCell>Phone No</TableCell>
+            <TableCell>Contact Person </TableCell>
+            <TableCell>Facilitator</TableCell>
+            <TableCell>Sites</TableCell>
+            <TableCell>Tenants</TableCell>
+            <TableCell>Tenant Group</TableCell>
+            <TableCell>Action </TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
@@ -73,7 +56,11 @@ export default function Orders() {
               <TableCell>{row.name}</TableCell>
               <TableCell>{row.shipTo}</TableCell>
               <TableCell>{row.paymentMethod}</TableCell>
-              <TableCell align="right">{`$${row.amount}`}</TableCell>
+              <TableCell >{row.amount}</TableCell>
+              <TableCell>{row.sites}</TableCell>
+              <TableCell>{row.Tenants}</TableCell>
+              <TableCell>{row.Group}</TableCell>
+              <TableCell><MoreVertIcon  className='action'/></TableCell>
             </TableRow>
           ))}
         </TableBody>
